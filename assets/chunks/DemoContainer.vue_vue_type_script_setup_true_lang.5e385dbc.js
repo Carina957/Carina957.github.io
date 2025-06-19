2e8eacfd067d747af5c65896cf3f125f5b621226
Carina957 <1512984584@qq.com>
2025-06-19 11:11:33 +0000

    Update main to output generated at dd9e7a1

--- assets/chunks/DemoContainer.vue_vue_type_script_setup_true_lang.5e385dbc.js
+++
@@ -1,1 +0,0 @@
-import{d as t,b as a,a4 as n,c as r,r as c,t as l,G as d,o as s}from"./framework.be90ce21.js";const _={class:"demo wide"},i={key:0,class:"error"},f=t({__name:"DemoContainer",setup(m){const e=a(null);return n(o=>{e.value=o}),(o,u)=>(s(),r("div",_,[c(o.$slots,"default"),e.value?(s(),r("div",i,l(e.value),1)):d("",!0)]))}});export{f as _};
